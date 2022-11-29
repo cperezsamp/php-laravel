e483ca36e4292a50268f97fb795951fb2876d468
Cesar Perez <cesarperezsampedro@gmail.com>
2022-11-30 00:32:15 +0100

    Creados models y controllers

---
+++ app/Models/Inscrito.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Inscritos extends Model
+{
+    use HasFactory;
+}
