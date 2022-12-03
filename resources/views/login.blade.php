09ed7097f50059aca5f676b9f1e35014019278a0
ipuyols <ipuyols@uoc.edu>
2022-12-03 08:26:28 +0100

    cambios en .env

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestor de Eventos</title>
+    <title>Gestor de Eventos Laravel</title>
 </head>
 <body>
     <div class="bd-example m-5">
---
+++ resources/views/usuarios.blade.php
@@ -0,0 +1,4 @@
+<x-layout.header>
+<div>hola</div>
+</x-layout>
+<x-layout.footer></x-layout>
